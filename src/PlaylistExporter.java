99958fa680bf34034e067073179d3f6ca5afe94c
Rudi Novik <rudracer@yahoo.de>
2017-10-08 17:32:52 +0200

    add PlaylistExporter.java

---
+++ src/PlaylistExporter.java
@@ -0,0 +1,18 @@
+/**
+ * Created by Rudi on 08.10.2017.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class PlaylistExporter extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
