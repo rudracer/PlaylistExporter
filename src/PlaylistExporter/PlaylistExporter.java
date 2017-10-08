f2b75311967dc3635da16ed5b6b3d7742a9f8649
Rudi Novik <rudracer@yahoo.de>
2017-10-08 21:16:54 +0200

    working main window

--- src/PlaylistExporter/PlaylistExporter.java
+++ src/PlaylistExporter/PlaylistExporter.java
@@ -4,16 +4,28 @@
 
 package PlaylistExporter;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class PlaylistExporter extends Application {
+
+    private Stage stage;
 
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage) {
-
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("PickPlaylist.fxml"));
+        Scene scene = new Scene(root);
+        stage = primaryStage;
+        stage.setTitle("PlaylistExporter");
+        stage.setScene(scene);
+        stage.show();
     }
 }
