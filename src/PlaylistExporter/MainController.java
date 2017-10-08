ed027585ad90c22b229fc54c26dc247f4d7f632a
Rudi Novik <rudracer@yahoo.de>
2017-10-08 22:27:05 +0200

    add extensionFilter to playlistChooser; add targetChooser

--- src/PlaylistExporter/MainController.java
+++ src/PlaylistExporter/MainController.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
 import java.io.File;
 
@@ -36,9 +37,20 @@ public class MainController {
 
     @FXML
     protected void playlistBtnClick(ActionEvent event) {
+        FileChooser.ExtensionFilter extensionFilter =
+                new FileChooser.ExtensionFilter("XML files (*.xml)", "*.xml");
         FileChooser chooser = new FileChooser();
         chooser.setTitle("Choose Playlist File");
+        chooser.getExtensionFilters().add(extensionFilter);
         playlist = chooser.showOpenDialog(playlistBtn.getScene().getWindow());
-        System.out.println(playlist.getPath());
+        playlistLbl.setText(playlist.getPath());
+    }
+
+    @FXML
+    protected void targetBtnClick(ActionEvent event) {
+        DirectoryChooser chooser = new DirectoryChooser();
+        chooser.setTitle("Choose Target Directory");
+        File target = chooser.showDialog(targetBtn.getScene().getWindow());
+        targetLbl.setText(target.getPath());
     }
 }
