52ce35eda269fa358d1506915f58486f431a6474
Rudi Novik <rudracer@yahoo.de>
2017-10-08 21:42:57 +0200

    first working filepicker

--- src/PlaylistExporter/MainController.java
+++ src/PlaylistExporter/MainController.java
@@ -1,9 +1,12 @@
 package PlaylistExporter;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.stage.FileChooser;
+import java.io.File;
 
 /**
  * Created by Rudi on 08.10.2017.
@@ -27,8 +30,15 @@ public class MainController {
     @FXML
     TextArea statusTxt;
 
+    //Attributes
+    File playlist;
+    File target;
 
-    protected void playlistBtnClick() {
-        
+    @FXML
+    protected void playlistBtnClick(ActionEvent event) {
+        FileChooser chooser = new FileChooser();
+        chooser.setTitle("Choose Playlist File");
+        playlist = chooser.showOpenDialog(playlistBtn.getScene().getWindow());
+        System.out.println(playlist.getPath());
     }
 }
